e9b2101f62794c1124ae407b00fe3fba4cb7c4c3
Tom Zhang <tommyawesome08@gmail.com>
2024-12-28 13:29:41 -0500

    pagination classes defined (not added to the views yet)

---
+++ orbitview/content/pagination.py
@@ -0,0 +1,44 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class PostsPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_description = "post_count" # /post_count={num}
+    max_page_size = 150
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
+
+
+class ArticlePagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_description = "article_count" # /post_count={num}
+    max_page_size = 150
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
+
+
+class UserListPagination(PageNumberPagination):
+    page_size = 50
+    page_query_description = "user_count"
+    max_per_size = 150
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
