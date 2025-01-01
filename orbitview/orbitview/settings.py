2349aec83d89710778794a0fb33f5b45a8d1ed6a
Tom Zhang <tommyawesome08@gmail.com>
2024-12-31 21:21:25 -0500

    ready for Python anywhere deployment

--- orbitview/orbitview/settings.py
+++ orbitview/orbitview/settings.py
@@ -108,11 +108,14 @@ CORS_ALLOW_CREDENTIALS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'HOST': os.getenv("DB_HOST"), 
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv("DB_PORT")
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
