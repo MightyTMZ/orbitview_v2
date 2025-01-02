80ab304112a7ee0ba77daee3231cdcdce059e0d1
Tom Zhang <tommyawesome08@gmail.com>
2025-01-01 20:25:48 -0500

    added a slash to the backend server for safety and peace of mind

--- orbitview_web/src/importantLinks.tsx
+++ orbitview_web/src/importantLinks.tsx
@@ -1,5 +1,5 @@
 const production = true;
 
 export const backendServer = production
-  ? "https://orbitview.pythonanywhere.com"
+  ? "https://orbitview.pythonanywhere.com/"
   : "http://127.0.0.1:8000/";
